08d13355b43fa0d3d8df43dcc3ad16c9cddc7c0a
¨pmaestre03¨ <maestrep15@gmail.com>
2023-10-07 09:43:52 +0200

    Delete Product

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const url = require('url')
 const port = 3000
 const ejs = require('ejs')
-const fs = require('fs/promises')
+const fs = require('fs').promises
 const { log } = require('console')
 app.set('view engine', 'ejs')
 
@@ -16,19 +16,25 @@ app.use(express.static('public'))
 // Configurar direcció ‘/’ 
 app.get('/', getProd)
 async function getProd(req, res) {
-  let query = url.parse(req.url, true).query;
-  let id = []
-  let nom = []
-  let id_nom = []
+    let query = url.parse(req.url, true).query;
+    let id = []
+    let nom = []
+    let id_nom = []
     try {
         let dadesArxiu = await fs.readFile("./private/productes.json", { encoding: 'utf8' })
+        
+        
         let dades = JSON.parse(dadesArxiu)
+        if (dades.length === 0) {
+            res.send('No products')
+            
+        }
         let infoProd = dades.find(prod => prod.nom)
         if (infoProd) {
-            nom = dades.map(prod => {return prod.nom})
-            id = dades.map(prod => {return prod.id})
-            res.render('sites/list', {llistaId: id,llistaNom: nom})
-        }
+            nom = dades.map(prod => { return prod.nom })
+            id = dades.map(prod => { return prod.id })
+            res.render('sites/list', { llistaId: id, llistaNom: nom })
+        } 
     }
     catch (error) {
         console.error(error)
@@ -36,20 +42,19 @@ async function getProd(req, res) {
     }
 }
 
-app.get('/edit',getEdit)
-async function getEdit(req,res) {
+app.get('/edit', getEdit)
+async function getEdit(req, res) {
     let query = url.parse(req.url, true).query;
     try {
         let dadesArxiu = await fs.readFile("./private/productes.json", { encoding: 'utf8' })
         let dades = JSON.parse(dadesArxiu)
-        console.log(dades) 
         let infoProd = dades.find(prod => (prod.id == query.id))
         if (infoProd) {
-            res.render('sites/products', {infoProd: infoProd})
+            res.render('sites/products', { infoProd: infoProd })
         }
         else {
             res.send('Paràmetres incorrectes')
-          }
+        }
     } catch (error) {
         console.log(error)
         res.send('Incorrecto')
@@ -62,18 +67,23 @@ async function getAdd(req, res) {
     res.send('Add product')
 }
 
-app.get('/delete',getDelete)
-async function getDelete(req,res) {
-    let arxiu = "./private/coets.json"
-    let postData = await getPostObject(req)
+app.get('/delete', getDelete)
+async function getDelete(req, res) {
+    
+    let arxiu = "./private/productes.json"
+    let query = url.parse(req.url, true).query;
     try {
-        // Llegir el fitxer JSON
         let dadesArxiu = await fs.readFile(arxiu, { encoding: 'utf8' })
         let dades = JSON.parse(dadesArxiu)
+        const dadesFiltrades = dades.filter((item) => item.id !== parseInt(query.id))
+        console.log(query)
+        const updateFile = JSON.stringify(dadesFiltrades)
+        await fs.writeFile(arxiu, updateFile)
+        res.redirect('/')
     } catch (error) {
         console.error(error)
-        res.send('Error al afegir les dades')
-      }
+        res.send('Error al eliminar el producte')
+    }
 }
 // Activar el servidor
 const httpServer = app.listen(port, appListen)
